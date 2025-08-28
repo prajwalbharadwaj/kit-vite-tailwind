341ffefd269f4e1f6d14c099ea974273b3d00f76
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-08-28 12:55:09 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="grid place-content-center h-screen font-semibold text-2xl">
+    Kit-vite-tailwind
+  </div>
+</template>
+
+<style scoped></style>
